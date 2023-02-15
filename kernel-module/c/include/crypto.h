5a3015b4d38488e3ee4076b5a1acc73588d54931
Qingsong Chen <changxian.cqs@antgroup.com>
2023-02-15 07:28:28 +0000

    add cbc(aes) skcipher util

--- kernel-module/c/include/crypto.h
+++ kernel-module/c/include/crypto.h
@@ -8,6 +8,8 @@
 #define DM_JINDISK_CRYPTO_H
 
 #include <crypto/aead.h>
+#include <crypto/skcipher.h>
+#include <linux/crc32.h>
 #include <linux/scatterlist.h>
 
 // in bytes
@@ -62,6 +64,50 @@ struct aes_gcm_cipher {
 	struct mutex lock;
 };
 
+/*
+ * name         : cbc(aes)
+ * driver       : cbc-aes-aesni
+ * module       : aesni_intel
+ * priority     : 400
+ * refcnt       : 1
+ * selftest     : passed
+ * internal     : no
+ * type         : skcipher
+ * async        : yes
+ * blocksize    : 16
+ * min keysize  : 16
+ * max keysize  : 32
+ * ivsize       : 16
+ * chunksize    : 16
+ * walksize     : 16
+ */
+
+#define AES_CBC_ENCRYPT 0
+#define AES_CBC_DECRYPT 1
+#define AES_CBC_BLOCK_SIZE 16
+#define AES_CBC_BLOCK_MASK (AES_CBC_BLOCK_SIZE - 1)
+#define AES_CBC_KEY_SIZE 16
+#define AES_CBC_IV_SIZE 16
+
+struct skcipher {
+	int (*encrypt)(struct skcipher *sc, char *data, int len, char *key,
+		       char *iv, uint64_t seq, char *out);
+	int (*decrypt)(struct skcipher *sc, char *data, int len, char *key,
+		       char *iv, uint64_t seq, char *out);
+	void (*destroy)(struct skcipher *sc);
+};
+
+struct aes_cbc_cipher {
+	struct skcipher skcipher;
+	struct crypto_skcipher *tfm;
+	struct skcipher_request *req;
+	size_t block_size;
+	size_t key_size;
+	size_t iv_size;
+	struct mutex lock;
+};
+
 struct aead_cipher *aes_gcm_cipher_create(void);
+struct skcipher *aes_cbc_cipher_create(void);
 
 #endif
